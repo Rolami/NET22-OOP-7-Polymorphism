c6b9551caf3d430ae6694c7463af4ea563ffe14b
Rolami <24991308+Rolami@users.noreply.github.com>
2022-10-19 16:37:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP7_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
